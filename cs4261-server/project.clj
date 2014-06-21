6c9786b792bc320fbba3866140fa78b5b94a9f6e
Michael Fender <mfender7@gatech.edu>
2014-06-21 18:22:56 -0400

    Can create the url needed to send request to Yumly. Prints that to screen instead of redirecting.

--- cs4261-server/project.clj
+++ cs4261-server/project.clj
@@ -3,9 +3,11 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [org.clojure/data.json "0.2.5"]]
+                 [org.clojure/data.json "0.2.5"]
+                 [http-kit "2.1.6"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-json "0.3.1"]
+                 [ring.middleware.jsonp "0.1.4"]]
   :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler cs4261-server.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :ring {:handler cs4261-server.handler/app})
--- cs4261-server/src/cs4261_server/handler.clj
+++ cs4261-server/src/cs4261_server/handler.clj
@@ -3,12 +3,28 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [clojure.data.json :as json]
-            [clojure.contrib.http.agent]))
+            [ring.util.response :as resp]
+            [org.httpkit.client :as http]
+            [org.httpkit.server :as hs]
+            [ring.middleware.json :refer [wrap-json-params]]
+            [ring.middleware.jsonp :refer [wrap-json-with-padding]]))
+
+(defn get-recipes [param]
+  (let [req (str "http://api.yummly.com/v1/api/recipes?_app_id=78dccce1&_app_key=87460fca330c28f52a9603ababd5a54f&allowedCourse[]=course^course-Dinner&maxTotalTimeInSeconds=3600&" param)]
+    (str req)))
+
 
 (defroutes app-routes
-  (GET "/recipes" [] (json/write-str { :message "This is some response"}))
+  (GET "/somerecipe" {params :query-string} (get-recipes params))
+  (GET "/recipes" [] (json/write-str { :message "This is some other different response"}))
   (route/resources "/")
-  (route/not-found "Not Found"))
+  (route/not-found "This isn't the page you're looking for.."))
 
 (def app
-  (handler/site app-routes))
+  (-> #'app-routes
+      (handler/api)
+      (wrap-json-params)))
+
+
+;http://api.yummly.com/v1/api/recipes?_app_id=78dccce1&_app_key=87460fca330c28f52a9603ababd5a54f&allowedCourse[]=course^course-Dinner&maxTotalTimeInSeconds=3600
+;your _search_parameters
