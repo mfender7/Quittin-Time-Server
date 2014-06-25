ed34c20a6fbb8b73517c30bb3d82b98cbcd12eb8
Michael Fender <mfender7@gatech.edu>
2014-06-24 21:37:09 -0400

    Can now get more than 1 random results!

--- cs4261-server/src/cs4261_server/handler.clj
+++ cs4261-server/src/cs4261_server/handler.clj
@@ -13,19 +13,26 @@
 (def base-get "http://api.yummly.com/v1/api/recipe/")
 (def base-search "http://api.yummly.com/v1/api/recipes?")
 (def api-key "_app_id=78dccce1&_app_key=87460fca330c28f52a9603ababd5a54f")
+(def base-query "&allowedCourse[]=course^course-Dinner&maxTotalTimeInSeconds=3600&")
 
-(defn get-recipes [id]
+(defn get-ids [request]
+  (json/read-str (get (client/get request) :body) :key-fn keyword))
+
+(defn get-recipe [id]
   (let [resp (client/get (str base-get id "?" api-key))]
     (let [body (json/read-str (get resp :body) :key-fn keyword)]
-      (json/write-str {:title id :ingredients (:ingredientLines body)}))
-    ))
+      (json/write-str {:title id, :ingredients (:ingredientLines body)}))))
+
+(defn get-random [matches]
+  (->> (shuffle matches)
+       (map :id)
+       (take 3)))
 
 (defn search-recipes [param]
-  (let [req (str base-search api-key "&allowedCourse[]=course^course-Dinner&maxTotalTimeInSeconds=3600&" param)]
-    (let [resp (client/get req)]
-      (let [body (json/read-str (get resp :body) :key-fn keyword)]
-        (let [matches (:matches body)]
-          (get-recipes (get (nth matches (rand-int 10)) :id)))))))
+  (let [req (str base-search api-key base-query param)
+        matches (:matches (get-ids req))]
+    (->> (get-random matches)
+         (map get-recipe))))
 
 (defroutes app-routes
   (GET "/" [] (json/write-str "This is just some start screen to appease myself."))
